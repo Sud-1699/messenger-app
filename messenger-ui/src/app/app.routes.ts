a50181b6d75c4201b9e09f6a2af538a2122da393
Sudhanshu Chaubey <sudhanshuchaubey397@gmail.com>
2026-04-19 15:43:22 +0530

    auth work begin

--- messenger-ui/src/app/app.routes.ts
+++ messenger-ui/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    },
+    {
+        title: 'Login',
+        path: 'login',
+        loadComponent: () => import("./login/login").then(c => c.Login)
+    },
+    {
+        title: 'Register',
+        path: 'register',
+        loadComponent: () => import("./registration/registration").then(c => c.Registration)
+    }
+];
---
+++ messenger-ui/src/app/login/login.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-login',
+  imports: [MatFormFieldModule, MatInputModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+
+}
